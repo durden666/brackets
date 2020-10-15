518fff65323fbbe5acdbd56ccc8d318f749d93f5
durden666 <thanks.vlad12@gmail.com>
2020-10-15 18:56:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  let brackets = config.map(a => a.join(''));
+
+  console.log(brackets);
+  
+  for (let i = 0; i < brackets.length;) {
+      if (str.includes(brackets[i])) {
+          str = str.replace(brackets[i], '');
+          i = 0;
+      } else {
+          i++;
+      }
+  }
+  return str.length === 0 ? true : false;
 }
